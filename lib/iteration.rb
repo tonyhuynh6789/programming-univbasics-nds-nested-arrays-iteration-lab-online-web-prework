5a3a80bb1e27a5497831fd11110d6e8d7d6bed4c
Tony Rim <duc_thinh_03@yahoo.com>
2019-10-31 22:37:00 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,20 +1,63 @@
+#require "pry"
+#binding.pry
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
+array = []
+row_index = 0
+
+while row_index < src.count do
+  items = src[row_index]
+  array << "I love #{items[0]} and #{items[1]} on my pizza"
+row_index += 1
+end 
+array
+end 
+
+
+ # # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
+ # #
   # Build a new Array that contains strings where each pair of foods is
   # inserted into this template:
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
-end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+  array = []
+  row_index = 0
+  
+  while row_index < src.count do
+    numbers = src[row_index]
+    if numbers[0] > numbers [1]
+      array << numbers[0]
+    else 
+      array << numbers[1]
+    end 
+    row_index +=1
+  end 
+  array 
+end 
+ # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
-end
-
+  
+  
+  
+  
+  
 def total_even_pairs(src)
+row_index = 0
+total = 0
+
+while row_index < src.length do
+  numbers = src[row_index]
+  if numbers[0] % 2 == 0 && numbers[1] % 2 == 0
+    total += numbers[0]
+    total += numbers[1]
+  end 
+  row_index += 1
+end 
+total
+end 
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
@@ -22,4 +65,4 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
-end
+
